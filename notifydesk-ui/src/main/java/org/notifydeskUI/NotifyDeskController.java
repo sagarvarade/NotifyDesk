d0cbe7d99b070d90920ec255d7428c93a1595a9c
sagar12 <varade.sagar12@gmail.com>
2025-12-07 22:42:51 +0530

    f2

--- notifydesk-ui/src/main/java/org/notifydeskUI/NotifyDeskController.java
+++ notifydesk-ui/src/main/java/org/notifydeskUI/NotifyDeskController.java
@@ -3,18 +3,18 @@ package org.notifydeskUI;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class NotifyDeskController {
 
-    //@RequestMapping(value = "/**/{[path:[^\\.]*}")
-    @RequestMapping(value = {"/ui/**", "/**/{path:[^\\.]*}"})
-    public ModelAndView home() {
-        return new ModelAndView("index");
+    // @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    @RequestMapping(value = { "/ui/**", "/**/{path:[^\\.]*}" })
+    public String home() {
+        return "forward:/index.html";
     }
+
     @GetMapping("/notify-desk")
-    public  String broadComUI(){
-        return  "Notify Desk UI up...";
+    public String broadComUI() {
+        return "Notify Desk UI up...";
     }
 }
